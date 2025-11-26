da120162a0be4516d2ba284c6f9f7eca202c8cd3
Blackcode2 <72853950+Blackcode2@users.noreply.github.com>
2025-11-27 00:44:00 +0900

    Time: 0 ms (100%), Space: 20.3 MB (16.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.swift
+++ 0001-two-sum/0001-two-sum.swift
@@ -12,11 +12,6 @@ class Solution {
             if dic[complement] != nil && dic[complement] != i {
                 return [i, dic[complement]!]
             }
-            // if(dic.values.contains(complement)){
-            //     let keys = dic.filter { $0.value == complement }.map { $0.key }
-            //     let filtered = keys.filter { $0 != i }
-            //     if(!filtered.isEmpty) {return [i, filtered[0]]}
-            // }
         }
 
         return result
