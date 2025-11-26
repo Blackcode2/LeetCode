f036f6a38fd0be9fdddf64b3980fcd6723bebf88
Blackcode2 <72853950+Blackcode2@users.noreply.github.com>
2025-11-27 00:37:20 +0900

    Time: 243 ms (5.52%), Space: 19.6 MB (78.23%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dic: [Int : Int] = [:]
+        var result: [Int] = []
+        
+        for (index, value) in nums.enumerated() {
+            dic[index] = value
+        }
+
+        for i in 0...nums.count-1 {
+            var complement = target - dic[i]!
+            if(dic.values.contains(complement)){
+                let keys = dic.filter { $0.value == complement }.map { $0.key }
+                let filtered = keys.filter { $0 != i }
+                if(!filtered.isEmpty) {return [i, filtered[0]]}
+            }
+        }
+
+        return result
+    }
+}
